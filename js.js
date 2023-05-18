a669dff8a896ed4b861868e64c82712c6888142a
desireebarretti <110750885+desireebarretti@users.noreply.github.com>
2023-05-18 20:23:21 -0300

    Botao e funcao reset criada

--- js.js
+++ js.js
@@ -1,7 +1,8 @@
 let botaoClick = document.querySelector('#btn__clickMe')
 let botaoCopy = document.querySelector('#btn__copy')
 let botaoReset = document.querySelector('#btn__reset')
-let espaçoMudaCor = document.querySelector('#ondeMudaraCor')
+let espaçoMudaCor = document.querySelector('#ondeMudaraCor') 
+let codigoHexadecimal = document.querySelector('#codigoHexadecimal')
 
 let hastag = '#'
 let cores = '0123456789ABCDEF'
@@ -9,8 +10,6 @@ let cores = '0123456789ABCDEF'
 botaoClick.style.display = 'block'
 
 function mudaBack() {
-    let codigoHexadecimal = document.querySelector('#codigoHexadecimal')
-
     codigoHexadecimal.style.display = 'block'
 
     for (let i = 0; i < 6; i++) {
@@ -22,18 +21,18 @@ function mudaBack() {
         espaçoMudaCor.style.background = imprimir
 
     }
-    
+
     botaoCopy.style.display = 'block'
     botaoClick.style.display = 'none'
     botaoReset.style.display = 'block'
-    
-    function reset() {
-        
-    }
     
 }
 
 function copiarCodigo() {
     
 
+}
+
+function reset() {
+    location.reload()
 }
