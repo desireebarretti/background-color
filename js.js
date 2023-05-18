3646047bac2abb7d873295592bf9ee58a82e2f3e
desireebarretti <110750885+desireebarretti@users.noreply.github.com>
2023-05-18 20:14:45 -0300

    Criando botao reset

--- js.js
+++ js.js
@@ -1,8 +1,12 @@
-let background = document.querySelector('#mudaCor')
 let botaoClick = document.querySelector('#btn__clickMe')
+let botaoCopy = document.querySelector('#btn__copy')
+let botaoReset = document.querySelector('#btn__reset')
+let espaçoMudaCor = document.querySelector('#ondeMudaraCor')
 
 let hastag = '#'
 let cores = '0123456789ABCDEF'
+
+botaoClick.style.display = 'block'
 
 function mudaBack() {
     let codigoHexadecimal = document.querySelector('#codigoHexadecimal')
@@ -13,7 +17,23 @@ function mudaBack() {
         let colorir = cores[Math.floor(Math.random() * 16)]
         console.log(colorir)
 
-        codigoHexadecimal.innerHTML = `${hastag += colorir}`
+        let imprimir = (codigoHexadecimal.innerHTML = `${hastag += colorir}`)
+
+        espaçoMudaCor.style.background = imprimir
+
+    }
+    
+    botaoCopy.style.display = 'block'
+    botaoClick.style.display = 'none'
+    botaoReset.style.display = 'block'
+    
+    function reset() {
         
     }
+    
+}
+
+function copiarCodigo() {
+    
+
 }
